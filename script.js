7474c7380a08da13c2dd76e92bd14114c6e47153
gustavoganhador07-sudo <gustavoganhador07@gmail.com>
2026-01-16 21:24:08 -0300

    codigo inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+var nome_pessoa, idade_pessoa;
+nome_pessoa = "gustavo";
+idade_pessoa = 17;
+
+console.log ("ola,o meu nome e " + nome_pessoa + " e tenho " + idade_pessoa + " de idade.");
+
+var num1 =10, num2 = 4, soma, subtracao, multiplicacao, divisao;
+soma = num1+num2;
+subtracao = num1-num2;
+multiplicacao = num1 * num2;
+divisao = num1/num2;
+
+console.log("a soma e igual a : " + soma);
+console.log("a subtracao e : " + subtracao);
+console.log("a multiplicaco e : " + multiplicacao);
+console.log("a divisao e : " + divisao);
