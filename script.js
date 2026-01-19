f197f7eb542b379002afc3d1417f8438d45345da
gustavoganhador07-sudo <gustavoganhador07@gmail.com>
2026-01-19 20:10:44 -0300

    novo codigos

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ console.log("a soma e igual a : " + soma);
 console.log("a subtracao e : " + subtracao);
 console.log("a multiplicaco e : " + multiplicacao);
 console.log("a divisao e : " + divisao);
+
+var nome_pessoa, idade_pessoa, altura_pessoa,  maioridade;
+ 
+nome_pessoa = "Gustavo";
+idade_pessoa = 20; 
+altura_pessoa = pessoa = 1.75;
+maioridade;
+//comparaçao de idade
+if (idade_pessoa >=18) {
+    maioridade = "sou maior de idade."
+    
+}
+ else {
+    maioridade = "sou menor de idade."
+    
+}
+console.log("meu nome e " + nome_pessoa +  " e " + maioridade)
+
+console.log("meu nome e " + nome_pessoa +  "e tenho " + altura_pessoa + "de altura");
+
+
+let numero = 1; // Comece a contagem em 1
+while (numero <= 10) { // Continue enquanto 'numero' for menor ou igual a 10
+    console.log(numero); // Imprime o número atual no console
+    numero++; // Incrementa o número
+}
+
+
